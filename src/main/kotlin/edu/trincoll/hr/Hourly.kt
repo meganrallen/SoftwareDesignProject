594d93b042d08013070231ecceaedf108c628dfc
Meg Allen <114270498+meganrallen@users.noreply.github.com>
2024-12-22 17:19:56 -0500

    Update Hourly.kt

--- src/main/kotlin/edu/trincoll/hr/Hourly.kt
+++ src/main/kotlin/edu/trincoll/hr/Hourly.kt
@@ -1,33 +1,14 @@
 package edu.trincoll.hr
 
-// Hourly class should extend the Employee class. That means
-// its constructor should take a name of type String and an id of type Int,
-// and pass them to the Employee constructor.
-//
-// It also should have:
-//   - a "rate" field of type Double
-//   - a "hours" field of type Double, with a default value of 80.0
-//
-// It should override the pay() method to return the rate times the hours.
-//
-// It should override the toString method to return a string with the
-// name, id, rate, and hours of the employee.
-
-// Hourly class extends Employee
-class Hourly(
+class Hourly (
     name: String,
     id: Int,
     val rate: Double,
-    var hours: Double = 80.0
+    val hours: Double = 80.0  // paid every two weeks
 ) : Employee(name, id) {
 
-    // Override pay method to return rate * hours
-    override fun pay(): Double {
-        return rate * hours
-    }
+    override fun pay() = rate * hours
 
-    // Override toString method to include name, id, rate, and hours
-    override fun toString(): String {
-        return "Name: $name, ID: $id, Rate: $rate, Hours: $hours"
-    }
+    override fun toString() =
+        "Hourly(name=$name, id=$id, rate=$rate, hours=$hours)"
 }
